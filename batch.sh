6ebc4b38b496bc42166bdb9bca67af7a4f5957bc
dhamanpreet-kaur <56942715+dhamanpreet-kaur@users.noreply.github.com>
2023-02-05 15:01:07 -0800

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -3,13 +3,12 @@
 #SBATCH -p willhies,gpu
 #SBATCH -c 12
 #SBATCH --mem=48GB 
-#SBATCH -t 72:00:00
+#SBATCH -t 48:00:00
 #SBATCH --gpus=1
 #SBATCH --constraint="[GPU_MEM:32GB|GPU_MEM:40GB]"
 #SBATCH -J cmr_s1
 
 # activate conda environment
-module load system mesa
 source activate dynamic
 
 echonet segmentation --save_video
