d9ebc9ebd96ee0c7c1529d4eef8228079dc5a086
dhamanpreet-kaur <56942715+dhamanpreet-kaur@users.noreply.github.com>
2023-02-05 14:55:49 -0800

    initial echonet dynamic commit

---
+++ batch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -p willhies,gpu
+#SBATCH -c 12
+#SBATCH --mem=48GB 
+#SBATCH -t 72:00:00
+#SBATCH --gpus=1
+#SBATCH --constraint="[GPU_MEM:32GB|GPU_MEM:40GB]"
+#SBATCH -J cmr_s1
+
+# activate conda environment
+conda activate dynamic
+
+echonet segmentation --save_video
+echonet video
+
+
+
