855627454da4b3ac99742074906a84a6bde29d59
dhamanpreet-kaur <56942715+dhamanpreet-kaur@users.noreply.github.com>
2023-02-21 12:48:53 -0800

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p willhies,gpu
 #SBATCH -c 12
-#SBATCH --mem=48GB 
+#SBATCH --mem=100GB 
 #SBATCH -t 6:00:00
 #SBATCH --gpus=1
 #SBATCH --constraint="[GPU_MEM:32GB|GPU_MEM:40GB]"
@@ -11,7 +11,7 @@
 # activate conda environment
 source activate dynamic
 
-echonet video --weights /scratch/users/dkaur24/dynamic/output/video/r2plus1d_18_32_2_pretrained/best.pt --run_test --num_epochs 1 --batch_size 10 --num_workers 1
+echonet video --weights /scratch/users/dkaur24/dynamic/output/video/r2plus1d_18_32_2_pretrained/best.pt --run_test --num_epochs 1 --batch_size 10 --num_workers 0
 
 
 
